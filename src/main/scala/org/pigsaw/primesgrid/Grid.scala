6e151a449a22d6cf72268db4fb95c204271730db
Nik Silver <nik@niksilver.com>
2015-09-15 15:59:36 +0100

    Started to get the cells out as text.

--- src/main/scala/org/pigsaw/primesgrid/Grid.scala
+++ src/main/scala/org/pigsaw/primesgrid/Grid.scala
@@ -6,4 +6,6 @@ package org.pigsaw.primesgrid
 class Grid(s: Seq[Int]) {
 
   def apply(i: Int, j: Int) = s(i) * s(j)
+
+  def textAt(i: Int, j: Int) = " " + apply(i, j).toString
 }
--- src/test/scala/org/pigsaw/primesgrid/GridTest.scala
+++ src/test/scala/org/pigsaw/primesgrid/GridTest.scala
@@ -8,10 +8,25 @@ import org.scalatest.ShouldMatchers
  */
 class GridTest extends FlatSpec with ShouldMatchers{
 
+  def cross[T](s1: Seq[T], s2: Seq[T]): Seq[(T,T)] = for { i <- s1; j <- s2 } yield (i, j)
+
   "apply" should "give the product as appropriate" in {
     val g = new Grid(Seq(7,5,4,9))
     g(0,3) should equal (7*9)
     g(1,2) should equal (5*4)
     g(3,1) should equal (9*5)
   }
+
+  "textAt" should "be spaces followed by the right number" in {
+    val g = new Grid(Seq(7,5,4,9))
+    g.textAt(0,3) should startWith (" ")
+    g.textAt(0,3).trim should equal ((7*9).toString)
+    cross(0 to 3, 0 to 3).foreach { c =>
+      val (i, j) = c
+      val prod = g(i, j)
+      g.textAt(i, j) should startWith (" ")
+      g.textAt(i, j).trim should equal (prod.toString)
+    }
+  }
+
 }
