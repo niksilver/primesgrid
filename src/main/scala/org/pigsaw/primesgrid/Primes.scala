070c26740d86fbd0bd1f4e5ef7fcb811ae4a92f6
Nik Silver <nik@niksilver.com>
2015-09-22 22:28:28 +0100

    Fixed appalling error (it didn't actually calculate primes!)

--- src/main/scala/org/pigsaw/primesgrid/Primes.scala
+++ src/main/scala/org/pigsaw/primesgrid/Primes.scala
@@ -9,7 +9,7 @@ object Primes {
     * a factor of any others in the stream.
     */
   def sieve(nums: Stream[Int]): Stream[Int] =
-    nums.head #:: (nums.tail filter { _ % nums.head != 0})
+    nums.head #:: sieve(nums.tail filter { _ % nums.head != 0})
 
   /** All primes, starting at 1. */
   val all = 1 #:: sieve(Stream.from(2))
--- src/test/scala/org/pigsaw/primesgrid/PrimesTest.scala
+++ src/test/scala/org/pigsaw/primesgrid/PrimesTest.scala
@@ -16,15 +16,15 @@ class PrimesTest extends FlatSpec with ShouldMatchers {
 
   it should "filter out at least one factor if there is one" in {
     val nums = Stream(3, 4, 5, 6, 7)
-    Primes.sieve(nums).toList should equal (List(3, 4, 5, 7))
+    Primes.sieve(nums).take(4).toList should not contain (6)
   }
 
   it should "give a good list of primes from 2" in {
     val nums = Stream.from(2)
-    Primes.sieve(nums).take(5).toList should equal (List(2, 3, 5, 7, 9))
+    Primes.sieve(nums).take(5).toList should equal (List(2, 3, 5, 7, 11))
   }
 
   "Primes.all" should "give all primes from 1" in {
-    Primes.all.take(7).toList should equal (List(1, 2, 3, 5, 7, 9, 11))
+    Primes.all.take(7).toList should equal (List(1, 2, 3, 5, 7, 11, 13))
   }
 }
